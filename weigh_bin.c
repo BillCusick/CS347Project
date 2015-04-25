03b32a268d74163ce6270800e32fa8b8afd74c34
BillCusick <wccusick@yahoo.com>
2015-04-24 20:33:23 -0400

    Revert "Initial Upload"
    This reverts commit 559a8763e8695375ed96e9f4416ee380855e25f0.

--- weigh_bin.c
+++
@@ -1,9 +0,0 @@
-#include "ticket.h"
-/*
-This code is just meant to send a value from the weighing
-bin to another process. Unfortunately, haven't implemented
-cross-process communication yet.
-*/
-int main() {
-	return 1;
-}
